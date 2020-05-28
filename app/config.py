0e215b74493db67880f16673dc6fff807efd9bd4
agusrichard <agus.richard21@gmail.com>
2020-05-28 20:10:34 +0700

    Restructure

---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.getenv('SQLALCHEMY_DATABASE_URI') + '.sqlite'
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from app import create_app
+import os
+from dotenv import load_dotenv
+from .app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=bool(os.getenv('DEVELOPMENT')))
