8b87ba9ed473125fbf091e0ec8ea2f17a63abbb2
agusrichard <agus.richard21@gmail.com>
2020-05-29 03:15:17 +0700

    Completing user's views

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'data.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/views/user/views.py
+++ app/views/user/views.py
@@ -1,4 +1,6 @@
-from flask import jsonify, request
+import datetime
+import jwt
+from flask import jsonify, request, current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import user
 from ... import db
@@ -7,43 +9,142 @@ from ...model import User
 
 @user.route('/auth/register', methods=['POST'])
 def register():
-    data = request.get_json()
-    print('register data', data)
+    try:
+        data = request.get_json()
 
-    if not data and not data['username'] and not data['password']:
-        return jsonify({'message': 'Please provide the required fields'})
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(                    
-        username=data['username'],       
-        email=data['email'],             
-        fullname=data['username'],       
-        password=hashed_password       
-    )
+        if not data or not data.get('email') or not data.get('password'):
+            return jsonify({'message': 'Please provide the required fields'}), 400
+        hashed_password = generate_password_hash(data['password'], method='sha256')
+        new_user = User(                    
+            username=data['username'],       
+            email=data['email'],             
+            fullname=data['username'],       
+            password=hashed_password       
+        )
 
-    db.session.add(new_user)
-    db.session.commit()
+        db.session.add(new_user)
+        db.session.commit()
 
-    return jsonify({
-        'success': True,
-        'message': 'Register success',
-        'data': {
-            'username': data['username'],
-            'email': data['email'],
-            'fullname': data['username']
-        }
-    })
+        return jsonify({
+            'success': True,
+            'message': 'Register success',
+            'data': {
+                'username': data['username'],
+                'email': data['email'],
+                'fullname': data['username']
+            }
+        })
+    except:
+        return jsonify({
+            'success': False,
+            'message': 'Internal error'
+        }), 500
 
 @user.route('/auth/login', methods=['POST'])
 def login():
-    data = request.get_json()
+    try:
+        data = request.get_json()
+        print('data', data)
+        print('email data', data.get('email'))
+        print('password data', data.get('password'))
 
-    return ''
+        if not data or not data.get('email') or not data.get('password'):
+            return jsonify({
+                'success': False,
+                'message': 'Please provide required fields'
+            }), 400
+
+        user = User.query.filter_by(email=data.get('email')).first()
+
+        if not user:
+            return jsonify({
+                'success': False,
+                'message': 'User not found'
+            }), 404
+
+        if check_password_hash(user.password, data.get('password')):
+            token = jwt.encode({
+                'id': user.id,
+                'email': user.email,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+            }, current_app.config['SECRET_KEY'])
+
+            return jsonify({
+                'success': True,
+                'message': 'Login success',
+                'data': {
+                    'id': user.id,
+                    'email': user.email,
+                    'password': user.password
+                },
+                'token': token.decode('UTF-8')
+            })
+
+        return jsonify({
+            'success': False,
+            'message': 'Failed to login'
+        }), 500
+
+    except:
+        return jsonify({
+            'success': False,
+            'message': 'Internal error'
+        }), 500
 
 @user.route('/user', methods=['GET'])
 def get_all_users():
-    return jsonify({'message': 'Is it working?'})
+    try:
+        users = User.query.all()
+
+        if not users:
+            return jsonify({
+                'success': False,
+                'message': 'Users not found'
+            }), 404
+
+        output = [{
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'password': user.password
+        } for user in users]
+
+        return jsonify({
+            'success': True,
+            'message': 'Success to get all users',
+            'data': output
+        })
+    except:
+        return jsonify({
+            'success': False,
+            'message': 'Internal error'
+        })
 
 @user.route('/user/<user_id>', methods=['GET'])
 def get_one_user(user_id):
-    return ''
+    try:
+        user = User.query.filter_by(id=user_id).first()
+
+        if not user:
+            return jsonify({
+                'success': False,
+                'message': 'User not found'
+            }), 404
+
+
+        return jsonify({
+            'success': True,
+            'message': 'Success to get user with id ' + user_id,
+            'data': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'password': user.password
+            }
+        })
+    except:
+        return jsonify({
+            'success': False,
+            'message': 'Internal error'
+        })
 
